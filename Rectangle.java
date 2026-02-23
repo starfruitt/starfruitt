384368a7588d2a8281aa9c97b75f48417d3c11a1
Johannely Rodriguez <153057456+starfruitt@users.noreply.github.com>
2026-02-23 17:11:19 -0600

    Area of Rectangle

---
+++ Rectangle.java
@@ -0,0 +1,45 @@
+public class Rectangle {
+    private double width = 1.0;
+    private double height = 1.0;
+
+    public Rectangle() {}
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getArea() {
+        return width * height;
+    }
+
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public static void main(String[] args) {
+        // Test the Rectangle class
+        Rectangle rect1 = new Rectangle(4, 40);
+        Rectangle rect2 = new Rectangle(3.5, 35.9);
+
+        System.out.println("Rectangle 1:");
+        System.out.println("Width: " + rect1.getWidth());
+        System.out.println("Height: " + rect1.getHeight());
+        System.out.println("Area: " + rect1.getArea());
+        System.out.println("Perimeter: " + rect1.getPerimeter());
+
+        System.out.println("Rectangle 2:");
+        System.out.println("Width: " + rect2.getWidth());
+        System.out.println("Height: " + rect2.getHeight());
+        System.out.println("Area: " + rect2.getArea());
+        System.out.println("Perimeter: " + rect2.getPerimeter());
+    }
+}
